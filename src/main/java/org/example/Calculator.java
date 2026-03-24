5d0e468546d198ba789f5289ce7cb9c8757fceeb
AnushaR1711 <anumanushru@gmail.com>
2026-03-24 12:27:45 +0530

    Initial commit of Gradle project

---
+++ src/main/java/org/example/Calculator.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class Calculator {
+
+    public int addtion(int a, int b) {
+        return a + b;
+    }
+        public int multiplication(int a,int b){
+            return a * b;
+        }
+            static void main() {
+                System.out.println("Hello Gradle");
+            }
+
+    public int addition(int i, int i1) {
+        return 0;
+    }
+}
